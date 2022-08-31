1f5bb51f9698f9bb9f2333a602ca45c2320f5727
SeongchaeChoi <112529225+Seongchae@users.noreply.github.com>
2022-08-31 11:05:46 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@ let 밝기 = 0
 basic.forever(function () {
     밝기 = pins.analogReadPin(AnalogPin.P1)
     if (밝기 > 800) {
-        pins.digitalWritePin(DigitalPin.P2, 45)
+        pins.servoWritePin(AnalogPin.P2, 45)
         basic.showIcon(IconNames.Happy)
     } else {
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.servoWritePin(AnalogPin.P2, 0)
         basic.showIcon(IconNames.Sad)
     }
 })
