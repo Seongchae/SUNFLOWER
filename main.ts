7ce050c099c71407c705efc41ce58c3f4f781b46
SeongchaeChoi <112529225+Seongchae@users.noreply.github.com>
2022-08-31 11:03:30 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 let 밝기 = 0
 basic.forever(function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
     밝기 = pins.analogReadPin(AnalogPin.P1)
     if (밝기 > 800) {
         pins.digitalWritePin(DigitalPin.P2, 45)
