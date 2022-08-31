b634e6d57beab9b5fe3bbc66a02cbff5c39ad464
Seongchae <112529225+Seongchae@users.noreply.github.com>
2022-08-31 10:56:34 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,12 @@
+let 밝기 = 0
+basic.forever(function () {
+    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    밝기 = pins.analogReadPin(AnalogPin.P1)
+    if (밝기 > 800) {
+        pins.digitalWritePin(DigitalPin.P2, 45)
+        basic.showIcon(IconNames.Happy)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 45)
+        basic.showIcon(IconNames.Sad)
+    }
+})
